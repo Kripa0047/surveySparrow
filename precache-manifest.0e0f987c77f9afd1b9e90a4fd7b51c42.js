117c7256d76b79e978798bdc10288d5519f21114
kripa0047 <kripabairagi0047@gmail.com>
2020-07-24 10:43:00 +0530

    Updates

---
+++ precache-manifest.0e0f987c77f9afd1b9e90a4fd7b51c42.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec30838d97c3ba18ac07d34e885eac1a",
+    "url": "/surveySparrow/index.html"
+  },
+  {
+    "revision": "51dea1a8fffc96736a8c",
+    "url": "/surveySparrow/static/css/main.30584dc1.chunk.css"
+  },
+  {
+    "revision": "65df2fa6448c325b696b",
+    "url": "/surveySparrow/static/js/2.766f39ba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/surveySparrow/static/js/2.766f39ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51dea1a8fffc96736a8c",
+    "url": "/surveySparrow/static/js/main.41c26c24.chunk.js"
+  },
+  {
+    "revision": "2dc86b04464c563a8bf0",
+    "url": "/surveySparrow/static/js/runtime-main.2c5d72f9.js"
+  },
+  {
+    "revision": "4a6ebd5ac6c39df826439199c960c8ef",
+    "url": "/surveySparrow/static/media/Banner.4a6ebd5a.png"
+  },
+  {
+    "revision": "ade91f473255991f410f61857696434b",
+    "url": "/surveySparrow/static/media/Montserrat-Bold.ade91f47.ttf"
+  },
+  {
+    "revision": "c8b6e083af3f94009801989c3739425e",
+    "url": "/surveySparrow/static/media/Montserrat-Medium.c8b6e083.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/surveySparrow/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "c641dbee1d75892e4d88bdc31560c91b",
+    "url": "/surveySparrow/static/media/Montserrat-SemiBold.c641dbee.ttf"
+  },
+  {
+    "revision": "b840e171a500c69863f00f6b4056b486",
+    "url": "/surveySparrow/static/media/close-24px.b840e171.svg"
+  },
+  {
+    "revision": "5613db24b64483f4b5f68fd55cc50f26",
+    "url": "/surveySparrow/static/media/menu-white-18dp.5613db24.svg"
+  },
+  {
+    "revision": "f2ec4b6cd3a578c2ae79b57d9f5b646f",
+    "url": "/surveySparrow/static/media/send-24px-color.f2ec4b6c.svg"
+  },
+  {
+    "revision": "cc48796f92dc3fc2363e19be730cabbf",
+    "url": "/surveySparrow/static/media/send-24px.cc48796f.svg"
+  }
+]);
